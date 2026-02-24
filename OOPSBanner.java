4d8110487a354aeb16ca53e149644f5d218e516a
vashininagalla <vashininagalla@email.com>
2026-02-24 14:11:16 +0530

    UC6: Implement static helper methods for O, P, S (DRY principle)

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -2,13 +2,53 @@ public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *****   ***** ");
-        System.out.println("*     * *     * *             *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    o[i] + "   " +
+                    o[i] + "   " +
+                    p[i] + "   " +
+                    s[i]
+            );
+        }
+    }
+
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    public static String[] buildP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
     }
 }
